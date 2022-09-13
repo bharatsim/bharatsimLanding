246ec8c390a46f40e74ee9b0761d44181572de9f
Yash More <yashmore@Yashs-MacBook-Pro.local>
2022-09-13 13:12:29 +0530

    Updates

---
+++ precache-manifest.a06298d7f92e394f75841361f7b00da8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa3687d2812dbbeedec9cddc47be240",
+    "url": "/bharatsimLanding/index.html"
+  },
+  {
+    "revision": "5f6e77a7d20d4c64461e",
+    "url": "/bharatsimLanding/static/css/main.871e4dae.chunk.css"
+  },
+  {
+    "revision": "10b0b4d8cad3dfff1b50",
+    "url": "/bharatsimLanding/static/js/2.522e2af1.chunk.js"
+  },
+  {
+    "revision": "5f6e77a7d20d4c64461e",
+    "url": "/bharatsimLanding/static/js/main.e2114162.chunk.js"
+  },
+  {
+    "revision": "4167ca0cf533ea00326c",
+    "url": "/bharatsimLanding/static/js/runtime~main.e8e93363.js"
+  },
+  {
+    "revision": "ed05fd719c64e1c06289e679986a2464",
+    "url": "/bharatsimLanding/static/media/Bhavesh_Neekhra_AU.ed05fd71.jpeg"
+  },
+  {
+    "revision": "39c419be46d054673216552c4518c1a2",
+    "url": "/bharatsimLanding/static/media/DebG.39c419be.jpeg"
+  },
+  {
+    "revision": "a9735e5dc4b2842517b4d586732434ad",
+    "url": "/bharatsimLanding/static/media/Logo_BW_on_dark.a9735e5d.svg"
+  },
+  {
+    "revision": "65e8f3fbf8331b14f5ed70b0330dfef7",
+    "url": "/bharatsimLanding/static/media/Logo_color_bg.65e8f3fb.png"
+  },
+  {
+    "revision": "539ed4cf0652e2a19a83fd2b3a1bde8b",
+    "url": "/bharatsimLanding/static/media/Philip_Profile.539ed4cf.jpg"
+  },
+  {
+    "revision": "431169163cefaa362615366e115c37b5",
+    "url": "/bharatsimLanding/static/media/chhaya.43116916.jpeg"
+  },
+  {
+    "revision": "1ce67d8c22380847ba4a95e342c7531f",
+    "url": "/bharatsimLanding/static/media/graph.1ce67d8c.png"
+  },
+  {
+    "revision": "5b638db190e349420e36b30ddd67b414",
+    "url": "/bharatsimLanding/static/media/harshal.5b638db1.png"
+  },
+  {
+    "revision": "053e22433c92fb3bed5135c4b8471028",
+    "url": "/bharatsimLanding/static/media/jayanta1.053e2243.jpg"
+  },
+  {
+    "revision": "dd34b16bc37bb1bc6f3d604c4a07f61f",
+    "url": "/bharatsimLanding/static/media/mumbai.dd34b16b.gif"
+  },
+  {
+    "revision": "dcd06fe8874dd73376e53068c0f11701",
+    "url": "/bharatsimLanding/static/media/nodes.dcd06fe8.png"
+  },
+  {
+    "revision": "3091001fc2cbb8917fe8d134c4f284f1",
+    "url": "/bharatsimLanding/static/media/person.3091001f.png"
+  },
+  {
+    "revision": "1ad1deb8b00249bf05b9ccd9998b5400",
+    "url": "/bharatsimLanding/static/media/praveen.1ad1deb8.jpg"
+  },
+  {
+    "revision": "6ffcf323d7b8a4166c9a92e847bbce93",
+    "url": "/bharatsimLanding/static/media/relations.6ffcf323.png"
+  },
+  {
+    "revision": "93c1f6cb7e11c657321de3d985c2fcc0",
+    "url": "/bharatsimLanding/static/media/schedules.93c1f6cb.png"
+  }
+]);
