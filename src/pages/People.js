76ca3c80d85aa1ef9771d992be4ccb3c030ad8f1
Philip Cherian <dpcherian@gmail.com>
2022-09-23 17:22:03 +0530

    Reduced filesize of images in people page.
    All these images are now 300x300 pixels, and jpgs.

--- src/pages/People.js
+++ src/pages/People.js
@@ -118,10 +118,10 @@ const People = () => {
 
       <Flex direction="row">
 
-      <ProfileCard name="Gautam Menon" title="Professor of Physics and Biology" img_url="https://www.ashoka.edu.in/wp-content/uploads/2021/04/Untitled-design-55.png" email="gautam.menon@ashoka.edu.in"/>
-      <ProfileCard name="Debayan Gupta" title="Assistant Professor of Computer Science" img_url={require('../assets/people/DebG.jpeg')} email="debayan.gupta@ashoka.edu.in"/>
-      <ProfileCard name="Bhavesh Neekhra" title="Graduate Student, Computer Science" img_url={require('../assets/people/Bhavesh_Neekhra_AU.jpeg')} email="bhavesh.neekhra_phd18@ashoka.edu.in"/>
-      <ProfileCard name="Philip Cherian" title="Graduate Student, Physics" img_url={require('../assets/people/Philip_Profile.jpg')} email="philip.cherian@ashoka.edu.in"/>
+      <ProfileCard name="Gautam Menon" title="Professor of Physics and Biology" img_url="../assets/people/au_gautam_menon.jpg" email="gautam.menon@ashoka.edu.in"/>
+      <ProfileCard name="Debayan Gupta" title="Assistant Professor of Computer Science" img_url={require('../assets/people/au_debayan_gupta.jpg')} email="debayan.gupta@ashoka.edu.in"/>
+      <ProfileCard name="Bhavesh Neekhra" title="Graduate Student, Computer Science" img_url={require('../assets/people/au_bhavesh_neekhra.jpg')} email="bhavesh.neekhra_phd18@ashoka.edu.in"/>
+      <ProfileCard name="Philip Cherian" title="Graduate Student, Physics" img_url={require('../assets/people/au_philip_cherian.jpg')} email="philip.cherian@ashoka.edu.in"/>
 
       </Flex>
 
@@ -134,10 +134,10 @@ const People = () => {
 
       <Flex direction="row">
 
-      <ProfileCard name="Jayanta Kshirsagar" title="Engineering for Research" img_url={require('../assets/people/jayanta1.jpg')} email="jayantak@thoughtworks.com"/>
-      <ProfileCard name="Praveen Ninawe" title="Engineering for Research" img_url={require('../assets/people/praveen.jpg')} email="praveen.ninawe@thoughtworks.com"/>
-      <ProfileCard name="Harshal Hayatnagarkar" title="Engineering for Research" img_url={require('../assets/people/harshal.png')} email="harshalh@thoughtworks.com"/>
-      <ProfileCard name="Chhaya Yadav" title="Engineering for Research" img_url={require('../assets/people/chhaya.jpeg')} email="chhayay@thoughtworks.com"/>
+      <ProfileCard name="Jayanta Kshirsagar" title="Engineering for Research" img_url={require('../assets/people/tw_jayanta_kshirsagar.jpg')} email="jayantak@thoughtworks.com"/>
+      <ProfileCard name="Praveen Ninawe" title="Engineering for Research" img_url={require('../assets/people/tw_praveen_ninawe.jpg')} email="praveen.ninawe@thoughtworks.com"/>
+      <ProfileCard name="Harshal Hayatnagarkar" title="Engineering for Research" img_url={require('../assets/people/tw_harshal_hayatnagarkar.jpg')} email="harshalh@thoughtworks.com"/>
+      <ProfileCard name="Chhaya Yadav" title="Engineering for Research" img_url={require('../assets/people/tw_chhaya_yadav.jpg')} email="chhayay@thoughtworks.com"/>
 
       </Flex>
 
